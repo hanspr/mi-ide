435682404a6d9632dd427cbc1f9d0f03f55d96af
Hans Peyrot <hans@eflow.com.mx>
2019-07-02 21:42:49 -0500

    removed autoclose from config dir

--- config/micro-ide/plugins/settings/settings.lua
+++ config/micro-ide/plugins/settings/settings.lua
@@ -5,12 +5,14 @@ function onViewOpen(view)
     local ft = view.Buf.Settings["filetype"]
     
     if  ft == "go" or
-        ft == "xslt" or
-        ft == "perl" or
+        ft == "perl" then
+        SetLocalOption("tabstospaces", "false", view)
+        SetLocalOption("smartindent", "true", view)
+    elseif ft == "xslt" or
         ft == "html" or
         ft == "makefile" then
         SetLocalOption("tabstospaces", "false", view)
-        SetLocalOption("tabindents", "true", view)
+        SetLocalOption("smartindent", "false", view)
     elseif ft == "fish" or
            ft == "python" or
            ft == "python2" or
@@ -18,5 +20,6 @@ function onViewOpen(view)
            ft == "yaml" or
            ft == "nim" then
         SetLocalOption("tabstospaces", "true", view)
+        SetLocalOption("smartindent", "false", view)
     end
 end
