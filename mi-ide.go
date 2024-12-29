661adafaca0dc96635c4cff126591e4d7c615b5f
Hans Peyrot <hans@eflow.com.mx>
2024-12-29 12:15:58 -0600

    cleanup

--- mi-ide.go
+++ mi-ide.go
@@ -679,45 +679,10 @@ func main() {
 					didAction = true
 				} else if Mouse.Click && Mouse.Button == 1 {
 					// Mouse 1 click events only
-					// if searching {
-					// Happened during a search lock, release Search
-					// ExitSearch(CurView())
-					// } else if y >= h-2 {
 					if y >= h-2 {
 						// Ignore clicks on status and message area
 						didAction = true
 					}
-					// if !didAction {
-					// 	num := CurView().Num
-					// 	// Set current view
-					// 	// We loop through each view in the current tab and make sure the current view
-					// 	// is the one being clicked in
-					// 	for _, v := range tabs[curTab].Views {
-					// 		if x >= v.x && x < v.x+v.Width && y >= v.y && y < v.y+v.Height {
-					// 			tabs[curTab].CurView = v.Num
-					// 			if num != v.Num {
-					// 				v.moveMousePosition(x, y)
-					// 			}
-					// 		}
-					// 	}
-					// }
-					// } else if button == tcell.WheelUp || button == tcell.WheelDown {
-					// 	// Scroll event
-					// 	didAction = true
-					// 	// Some terminal send the event twice others not!
-					// 	// Patch for that, don't know in how many it will work
-					// 	if strings.Count(e.EscSeq(), "[") > 1 {
-					// 		break
-					// 	}
-					// 	var view *View
-					// 	for _, v := range tabs[curTab].Views {
-					// 		if x >= v.x && x < v.x+v.Width && y >= v.y && y < v.y+v.Height {
-					// 			view = tabs[curTab].Views[v.Num]
-					// 		}
-					// 	}
-					// 	if view != nil {
-					// 		view.HandleEvent(e)
-					// 	}
 				} else {
 					if y >= h-1 {
 						// Ignore moves at the message area
