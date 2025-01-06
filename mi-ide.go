a4553585ed78e63ec6b2d477b6b703d3695ecdb7
Hans Peyrot <hans@eflow.com.mx>
2025-01-05 22:58:59 -0600

    version update

--- mi-ide.go
+++ mi-ide.go
@@ -69,7 +69,7 @@ var (
 
 	// Version is the version number or commit hash
 	// These variables should be set by the linker when compiling
-	Version = "1.1.12"
+	Version = "1.1.13"
 	// CommitHash is the commit this version was built on
 	CommitHash = "Unknown"
 	// CompileDate is the date this binary was compiled on
