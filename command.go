569f365cf7d0c8ff682ea3682e33d7d63847aba6
Hans Peyrot <hans@eflow.com.mx>
2025-01-11 20:08:18 -0600

    fix: recover function

--- command.go
+++ command.go
@@ -362,6 +362,31 @@ func Eval(args []string) {
 	}
 }
 
+// NewTab opens the given file in a new tab
+func NewTab(args []string) {
+	if len(args) == 0 {
+		CurView().AddTab(true)
+	} else {
+		buf, err := NewBufferFromFile(args[0])
+		if err != nil {
+			messenger.Alert("error", err)
+			return
+		}
+
+		tab := NewTabFromView(NewView(buf))
+		tab.SetNum(len(tabs))
+		tabs = append(tabs, tab)
+		curTab = len(tabs) - 1
+		if len(tabs) == 2 {
+			for _, t := range tabs {
+				for _, v := range t.Views {
+					v.AddTabbarSpace()
+				}
+			}
+		}
+	}
+}
+
 // Set sets an option
 func Set(args []string) {
 	if len(args) < 2 {
