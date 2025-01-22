873431b21df37db06498486af8a3b120e7e5a0bc
Hans Peyrot <hans@eflow.com.mx>
2025-01-22 09:09:47 -0600

    version update

--- mi-ide.go
+++ mi-ide.go
@@ -68,7 +68,7 @@ var (
 
 	// Version is the version number or commit hash
 	// These variables should be set by the linker when compiling
-	Version = "1.1.47"
+	Version = "1.1.48"
 
 	// The list of views
 	tabs []*Tab
