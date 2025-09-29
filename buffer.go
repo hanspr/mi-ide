666f65c1581b3ed081f734e2f5ff0126bcdd70e2
Hans Peyrot <hans@eflow.com.mx>
2025-09-29 16:37:46 -0600

    begin autocomplete

--- buffer.go
+++ buffer.go
@@ -239,7 +239,8 @@ func NewBuffer(reader io.Reader, size int64, path string) *Buffer {
 		buf: b,
 	}
 	InitLocalSettings(b)
-
+	cc.loadCompletions(b.FileType())
+	TermMessage("Regresé")
 	b.cursors = []*Cursor{&b.Cursor}
 	b.pasteLoc.X = -1
 	return b
